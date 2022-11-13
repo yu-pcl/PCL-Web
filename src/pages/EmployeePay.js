d3acfe5cf18847fa27d070419161d92c44e7cc82
SorinKwon <ksl020700@gmail.com>
2022-11-13 13:05:59 +0900

    feat: url 수정

--- src/pages/EmployeePay.js
+++ src/pages/EmployeePay.js
@@ -61,7 +61,7 @@ const EmployeePay = () => {
                 </Top>
                     <StatementPage />
                 <Bottom>
-                    <button onClick={() => window.open('http://localhost:3000/statement', '_blank')}>전자명세서 발급</button>
+                    <button onClick={() => window.open('http://acslab.toygoon.com:8080/', '_blank')}>전자명세서 발급</button>
                 </Bottom>
             </Content>
         </Container>
