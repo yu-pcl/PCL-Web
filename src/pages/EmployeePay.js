7abfd1c150f6b7d49cceca5ba1761d4850ff43bb
Uichan <cksxkr5193@naver.com>
2022-11-17 18:45:06 +0900

    진짜 최종

--- src/pages/EmployeePay.js
+++ src/pages/EmployeePay.js
@@ -5,6 +5,7 @@ import StatementPage from './StatementPage';
 import { getCookie, setCookie } from './Cooke';
 import arrow_left from '../assets/arrow_left.png';
 import arrow_right from '../assets/arrow_right.png';
+import axios from 'axios';
 
 const dummy = {
     "year" : 2022,
@@ -13,11 +14,20 @@ const dummy = {
 
 const EmployeePay = () => {
     let fullname=getCookie("fullname");
-    let worker_id = getCookie("worker_id");
+    let id = getCookie("worker_id");
     setCookie("search_id", getCookie("worker_id"));
     const [year, setYear] = useState(dummy.year);
     const [month, setMonth] = useState(dummy.month);
     const [yearMonth, setYearMonth] = useState(dummy.year.toString()+dummy.month.toString());
+    const [basicPay, setBasicPay] = useState([]);
+    const [deliveryFee, setDeliveryFee] = useState([]);
+    const [pension, setPension] = useState([]);
+    const [health, setHealth] = useState([]);
+    const [employment, setEmployment] = useState([]);
+    const [occupational, setOccupational] = useState([]);
+    const [payTotal, setPayTotal] = useState([]);
+    const [insuranceTotal, setInsuranceTotal] = useState([]);
+    const [total, setTotal] = useState([]);
 
     const handleClickLeftButton = ()=>{
         if (month === 1){
@@ -39,16 +49,38 @@ const EmployeePay = () => {
 
     useEffect(() => {
         setYearMonth(year.toString()+month.toString());
-        setCookie("yearMonth", yearMonth);
-        console.log(yearMonth);
     }, [month])
+
+    useEffect(() => {
+        console.log(id);
+        console.log(yearMonth);
+        setCookie("yearMonth", yearMonth);
+        axios.post(`http://acslab.toygoon.com:8000/api/employ/${id}`,
+            {
+                month : yearMonth
+            })
+            .then(function(res){
+                setBasicPay(res.data.money_base);
+                setDeliveryFee(res.data.money_parcel);
+                setPension(res.data.money_pension);
+                setHealth(res.data.money_health);
+                setEmployment(res.data.money_employ);
+                setOccupational(res.data.money_accident);
+                setPayTotal(res.data.money_base+res.data.money_parcel);
+                setInsuranceTotal(res.data.money_pension+res.data.money_health+res.data.money_employ+res.data.money_accident);
+                setTotal(res.data.money_base+res.data.money_parcel-(res.data.money_pension+res.data.money_health+res.data.money_employ+res.data.money_accident));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [yearMonth]);
 
     return (
         <Container>
             <Content>
                 <Top>
                     <div>
-                        <h4 className='number'>{worker_id}</h4>
+                        <h4 className='number'>{id}</h4>
                         <h4>{fullname} 님</h4>
                     </div>
                     <div>
@@ -60,9 +92,65 @@ const EmployeePay = () => {
                         <button onClick={handleClickRightButton}>다음<img src={arrow_right}/></button>
                     </div>
                 </Top>
-                    <StatementPage />
+                <Box>
+                <div className='boxTop'>
+                    <div className='boxLeft'>
+                        <div className='text'>
+                            <p className='title'>지급액</p>
+                            <div className='moneyBox'>
+                                <Money>
+                                    <p>기본급</p>
+                                    <p>{basicPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>택배비</p>
+                                    <p>{deliveryFee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                            </div>
+                            <Money>
+                                <p>급여계</p>
+                                <p>{payTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            </Money>
+                        </div>
+                    </div>
+                    <div className='boxRight'>
+                        <div className='text'>
+                            <p className='title'>공제액</p>
+                            <div className='moneyBox'>
+                                <Money>
+                                    <p>국민연금</p>
+                                    <p>{pension.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>건강보험</p>
+                                    <p>{health.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>고용보험</p>
+                                    <p>{employment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>산재보험</p>
+                                    <p>{occupational.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                            </div>
+                            <Money>
+                                <p>공제합계</p>
+                                <p>{insuranceTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            </Money>
+                        </div>
+                    </div>
+                </div>
+                <div className='boxBottom'>
+                    <div>
+                        <p>차감수령액</p>
+                        <p>{total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    </div>
+                </div>
+            </Box>
                 <Bottom>
                     <button onClick={() => window.open('http://acslab.toygoon.com:8080/statement', '_blank')}>전자명세서 발급</button>
+                    {/* <button onClick={() => window.open('http://localhost:3000/statement', '_blank')}>전자명세서 발급</button> */}
                 </Bottom>
             </Content>
         </Container>
@@ -116,6 +204,73 @@ const Top = styled.div`
         }
     }
 `
+
+const Box = styled.div`
+    width: 60vw;
+    height: 65vh;
+    display : flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 0px 3px 6px #00000029;
+
+    .boxTop{
+        display : flex;
+        padding-top: 2vh;
+        
+        .boxLeft{
+            width: 30vw;
+            height: 54vh;
+            display : flex;
+            justify-content: center;
+            border-right: 1px solid #000000B5;
+        }
+        .boxRight{
+            width: 30vw;
+            height: 54vh;
+            display : flex;
+            justify-content: center;
+        }
+        .text{
+            width: 26vw;
+            height: 50vh;
+            display : flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            .title{
+                align-self: flex-start;
+            }
+            .moneyBox{
+                height: 25vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+            }
+        }
+    }
+    
+    .boxBottom{
+        width: 56vw;
+        height: 8vh;
+        display : flex;
+        align-items: center;
+        border-top: 1px solid #000000B5;
+
+        div{
+            width: 56vw;
+            display : flex;
+            justify-content: space-between;
+        }
+    }
+`
+const Money = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding-top: 2vh;
+`
+
 const Bottom = styled.div`
     align-self:end;
 
--- src/pages/ManagerPay.js
+++ src/pages/ManagerPay.js
@@ -16,7 +16,6 @@ const dummy = {
 const ManagerPay = () => {
     useEffect(()=>{
         setCookie("search_id", getCookie("worker_id"));
-        console.log("망함");
     }, []);
     const [id, setId] = useState(getCookie("worker_id"));
     const [name, setName] = useState(getCookie("fullname"));
@@ -24,6 +23,15 @@ const ManagerPay = () => {
     const [year, setYear] = useState(dummy.year);
     const [month, setMonth] = useState(dummy.month);
     const [yearMonth, setYearMonth] = useState(dummy.year.toString()+dummy.month.toString());
+    const [basicPay, setBasicPay] = useState([]);
+    const [deliveryFee, setDeliveryFee] = useState([]);
+    const [pension, setPension] = useState([]);
+    const [health, setHealth] = useState([]);
+    const [employment, setEmployment] = useState([]);
+    const [occupational, setOccupational] = useState([]);
+    const [payTotal, setPayTotal] = useState([]);
+    const [insuranceTotal, setInsuranceTotal] = useState([]);
+    const [total, setTotal] = useState([]);
 
     const handleClickLeftButton = ()=>{
         if (month === 1){
@@ -45,7 +53,6 @@ const ManagerPay = () => {
 
     useEffect(() => {
         setYearMonth(year.toString()+month.toString());
-        setCookie("yearMonth", yearMonth);
     }, [month])
 
     const onNameHandler=(event)=>{
@@ -64,6 +71,29 @@ const ManagerPay = () => {
             setId(response.data.user.worker_id);
         })
     }
+
+    useEffect(() => {
+        console.log(id);
+        console.log(yearMonth);
+        axios.post(`http://acslab.toygoon.com:8000/api/employ/${id}`,
+            {
+                month : yearMonth
+            })
+            .then(function(res){
+                setBasicPay(res.data.money_base);
+                setDeliveryFee(res.data.money_parcel);
+                setPension(res.data.money_pension);
+                setHealth(res.data.money_health);
+                setEmployment(res.data.money_employ);
+                setOccupational(res.data.money_accident);
+                setPayTotal(res.data.money_base+res.data.money_parcel);
+                setInsuranceTotal(res.data.money_pension+res.data.money_health+res.data.money_employ+res.data.money_accident);
+                setTotal(res.data.money_base+res.data.money_parcel-(res.data.money_pension+res.data.money_health+res.data.money_employ+res.data.money_accident));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }, [id, yearMonth]);
 
 
     return (
@@ -93,10 +123,65 @@ const ManagerPay = () => {
                         <button onClick={handleClickRightButton}>다음<img src={arrow_right}/></button>
                     </div>
                 </Top>
-                <StatementPage />
+                <Box>
+                <div className='boxTop'>
+                    <div className='boxLeft'>
+                        <div className='text'>
+                            <p className='title'>지급액</p>
+                            <div className='moneyBox'>
+                                <Money>
+                                    <p>기본급</p>
+                                    <p>{basicPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>택배비</p>
+                                    <p>{deliveryFee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                            </div>
+                            <Money>
+                                <p>급여계</p>
+                                <p>{payTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            </Money>
+                        </div>
+                    </div>
+                    <div className='boxRight'>
+                        <div className='text'>
+                            <p className='title'>공제액</p>
+                            <div className='moneyBox'>
+                                <Money>
+                                    <p>국민연금</p>
+                                    <p>{pension.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>건강보험</p>
+                                    <p>{health.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>고용보험</p>
+                                    <p>{employment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                                <Money>
+                                    <p>산재보험</p>
+                                    <p>{occupational.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </Money>
+                            </div>
+                            <Money>
+                                <p>공제합계</p>
+                                <p>{insuranceTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            </Money>
+                        </div>
+                    </div>
+                </div>
+                <div className='boxBottom'>
+                    <div>
+                        <p>차감수령액</p>
+                        <p>{total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    </div>
+                </div>
+            </Box>
             <Bottom>
-                {/* <button onClick={() => window.open('http://acslab.toygoon.com:8080/statement', '_blank')}>전자명세서 발급</button> */}
-                <button onClick={() => window.open('http://localhost:3000/statement', '_blank')}>전자명세서 발급</button>
+                <button onClick={() => window.open('http://acslab.toygoon.com:8080/statement', '_blank')}>전자명세서 발급</button>
+                {/* <button onClick={() => window.open('http://localhost:3000/statement', '_blank')}>전자명세서 발급</button> */}
             </Bottom>
             </Content>
         </Container>
@@ -177,6 +262,72 @@ const Top = styled.div`
             height:20px;
         }
     }
+`
+
+const Box = styled.div`
+    width: 60vw;
+    height: 65vh;
+    display : flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 0px 3px 6px #00000029;
+
+    .boxTop{
+        display : flex;
+        padding-top: 2vh;
+        
+        .boxLeft{
+            width: 30vw;
+            height: 54vh;
+            display : flex;
+            justify-content: center;
+            border-right: 1px solid #000000B5;
+        }
+        .boxRight{
+            width: 30vw;
+            height: 54vh;
+            display : flex;
+            justify-content: center;
+        }
+        .text{
+            width: 26vw;
+            height: 50vh;
+            display : flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            .title{
+                align-self: flex-start;
+            }
+            .moneyBox{
+                height: 25vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+            }
+        }
+    }
+    
+    .boxBottom{
+        width: 56vw;
+        height: 8vh;
+        display : flex;
+        align-items: center;
+        border-top: 1px solid #000000B5;
+
+        div{
+            width: 56vw;
+            display : flex;
+            justify-content: space-between;
+        }
+    }
+`
+const Money = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding-top: 2vh;
 `
 
 const Bottom = styled.div`
